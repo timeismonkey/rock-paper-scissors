24ee632252863e3a142d557c742653d7b7363f64
Jerry Hara <jerrykeiko98@gmail.com>
2023-11-30 21:50:50 -0800

    Added winning logic. Once a the user or computer wins a popup will show the winner.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Display score
-let playerScoreElement = document.querySelector(".player-score");
-let computerScoreElement = document.querySelector(".computer-score");
 let playerScore = 0;
 let computerScore = 0;
-playerScoreElement.textContent = playerScore;
-computerScoreElement.textContent = computerScore;
+let winner;
 
  // Randomly generate a choice between: Rock, Paper and Scissors
  function getComputerChoice(){
@@ -21,24 +17,43 @@ function playRound(playerChoice, computerChoice) {
     computerChoice = computerChoice.toLowerCase()
 
     // Computer wins
-    if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors")
-    || (playerChoice === "scissors" && computerChoice === "rock") ) {
+    if (
+        (playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "rock")
+    ) {
         // Increment the score of winning party
         computerScore++;
-        computerScoreElement.textContent = computerScore;
         return `Computer wins this round! ${computerChoice} beats ${playerChoice}.`;
     }
     // Player wins
-    else if (playerChoice === "rock" && computerChoice === "scissors" || (playerChoice === "paper" && computerChoice === "rock")
-    || (playerChoice === "scissors" && computerChoice=== "paper")) {
+    else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice=== "paper")
+    ) {
         playerScore++;
-        playerScoreElement.textContent = playerScore;
         return `Player wins this round! ${playerChoice} beats ${computerChoice}.`;
     }
     else {
         return "Tie"; 
     }
 }
+
+// Check for winner
+function checkForWinner() {
+    return computerScore === 5 || playerScore === 5
+}
+
+// Get winner
+function getWinner () {
+    if (computerScore === 5) {
+        return "computer"
+    }
+    return "player"
+}
+
+// Reset game
 
 // Simulate playing a round when a button is pressed
 const buttons = document.querySelectorAll("button");
@@ -47,31 +62,32 @@ buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         const computerChoice = getComputerChoice();
         const playerChoice = event.target.textContent;
-        const result = document.querySelector(".result");
-    
-        // Display results
-        if (computerScore !== 5 && computerScore !== 5) {
-            result.textContent = playRound(playerChoice, computerChoice);
-        }
+        const resultElement = document.querySelector(".result");
+        const result = playRound(playerChoice, computerChoice);
+        const playerScoreElement = document.querySelector(".player-score");
+        const computerScoreElement = document.querySelector(".computer-score");
+        playerScoreElement.textContent = playerScore;
+        computerScoreElement.textContent = computerScore;
 
-        // Check for winner
-        if (computerScore === 5) {
-            result.textContent = "Computer Wins!";
-            // Reset scores
-            let playerScore = 0;
-            let computerScore = 0;
-            playerScoreElement.textContent = playerScore;
-            computerScoreElement.textContent = computerScore;
+        // Check if winner
+        if (checkForWinner()) {
+            if (getWinner() === "computer"){
+                winner = "Computer wins!"
+                setTimeout(() => {
+                    window.alert("Computer wins!")
+                }, 0);
+            } else {
+                winner = "You win!"
+                setTimeout(() => {
+                    window.alert("Player wins!")
+                }, 0);
+            }
+            // End game
+        } else {
+            // resultElement.textContent = result;
+            winner = result;
         }
-
-        // Check for winner
-        if (playerScore === 5) {
-            result.textContent = "Player Wins!";
-            let playerScore = 0;
-            let computerScore = 0;
-            playerScoreElement.textContent = playerScore;
-            computerScoreElement.textContent = computerScore;
-        }
+        
+        resultElement.textContent = winner;
     })
 })
-// Keep simulating a round until a player reaches 5 points
